6b8c830249a77f790311b67751c983af073e6251
黄敏争 <mzhuang@storemax.cn>
2017-05-10 17:13:35 +0800

    NumberAnimTextView

---
+++ paowang/src/com/paowang/activity/NumberAnimTextViewActivity.java
@@ -0,0 +1,36 @@
+package com.paowang.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import me.bakumon.numberanimtextview.NumberAnimTextView;
+
+
+public class NumberAnimTextViewActivity extends AppCompatActivity {
+
+    private NumberAnimTextView mNumberAnimTextView;
+    private NumberAnimTextView mNumberAnimTextView1;
+    private NumberAnimTextView mNumberAnimTextView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_number_anim_textview);
+        mNumberAnimTextView = (NumberAnimTextView) findViewById(R.id.text);
+        mNumberAnimTextView1 = (NumberAnimTextView) findViewById(R.id.text1);
+        mNumberAnimTextView2 = (NumberAnimTextView) findViewById(R.id.text2);
+    }
+
+    public void start(View view) {
+        mNumberAnimTextView.setPrefixString("¥");
+        mNumberAnimTextView.setNumberString("99998.123456789");
+
+//        mNumberAnimTextView1.setEnableAnim(true);
+        mNumberAnimTextView1.setNumberString("1234567");
+
+        mNumberAnimTextView2.setPostfixString("%");
+        mNumberAnimTextView2.setNumberString("99.75");
+
+    }
+}
